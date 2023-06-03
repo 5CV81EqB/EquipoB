7c7a30ae9119ddb25cc0bb54aeed0a1471d0de50
Rafael Sardaneta Rendon <rafasarren0415@gmail.com>
2023-06-03 09:19:46 -0600

    archivos de configuracion

---
+++ clases/database.php
@@ -0,0 +1,71 @@
+<?php
+$server = ini_get('mysqli.default_host');
+if(!defined('SERVER')) define("SERVER", !empty($server) ? $server : '127.0.0.1');
+//Nombre de usuario
+$db_user = ini_get('mysqli.default_user');
+if(!defined("DB_USER")) define("DB_USER", !empty($db_user) ? $db_user : 'root');
+//Contraseña de acceso
+$db_pw = ini_get('mysqli.default_pw');
+if(!defined("PASSWORD")) define("PASSWORD", !empty($db_pw) ? $db_pw : NULL);
+
+
+class DATABASE{
+    protected $conexMySQL = NULL;
+    private $driver = NULL;
+    private $sql = NULL;
+    private $results = NULL;
+    /* Constructor
+        Inicia la conexion a la base de datos
+    */
+    function __construct($server = SERVER, $user = DB_USER, $password = PASSWORD, $database = DATABASE, $port = DB_PORT, $socket = DB_SOCKET) {
+        $this->driver = new mysqli_driver();
+        $this->driver->report_mode = MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT;
+        // Verifica acceso al servidor conexMySQL
+        $this->conexMySQL = new mysqli($server, $user, $password, $database, $port, $socket);
+        if ($this->conexMySQL->connect_errno) {
+            throw new Exception("Error de conexion: %s\n", $this->conexMySQL->connect_error);
+        } //if
+    } 
+    /* Destructor
+        Cierra la conexion a la base de datos
+    */
+    function __destruct() {
+        if(!is_null($this->conexMySQL))
+            $this->conexMySQL->close();
+        $this->driver->report_mode = MYSQLI_REPORT_OFF;
+    }
+    public function __get($fieldname) {
+        switch($fieldname) {
+            case 'ConexDB': return $this->conexMySQL;
+        } //switch
+
+        $trace = debug_backtrace();
+        throw new Exception(
+            'Undefined property via __get(): ' . $fieldname .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
+    }
+
+    public function error() {
+        $error = print_r($this->conexMySQL->error, TRUE);
+        $errno = $this->conexMySQL->errno;
+        return "Code: $errno -> SQL: $this->sql --> Error: $error";
+    }
+    /* Funcion query
+        aqui es donde se realizan las operaciones sql y devuelve el valor obtenido
+    */
+    public function query($sql) {
+        $this->sql = trim($sql);
+        if($this->debug) trigger_error($this->sql);
+        $this->results = $this->conexMySQL->query($this->sql);
+        if($this->conexMySQL->error) {
+            trigger_error($this->error());
+            throw new Exception($this->error(), $this->conexMySQL->errno);
+        } //if
+
+        return $this->results;
+    }
+}
+?>
---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Definicion de valores para la base de datos
+*/
+if(!defined("DB_USER")) define("DB_USER", "root");
+//Contraseña de acceso
+if(!defined("PASSWORD")) define("PASSWORD", "");
+//Schema o Base de datos por omisión
+if(!defined("DATABASE")) define("DATABASE", "TallerMecanico");
+ini_set('error_log',"C:/xampp/htdocs/priceshoes/psh_web_reclutamiento/errores/php_errors.log"); 
+?>
